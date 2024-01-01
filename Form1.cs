9a6e18750a4b78be353b21cbd368e726ec151c88
Pio <pio.cho.bia@gmail.com>
2024-01-01 22:12:18 +0100

    wersja 2

--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,6 @@
 namespace WinForms231231_grafikaGDI
-{
-    using System.Drawing;
-    using System.Drawing.Drawing2D;
+{ 
+
     public partial class Form1 : Form
     {      
 
@@ -12,31 +11,39 @@ namespace WinForms231231_grafikaGDI
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
-        {         
-            //stworzenie obiektu Graphics samodzielnie
-            Graphics gSamodzielnie = this.CreateGraphics();
-            
+        {                      
             //stworzenie obiektu Graphics z e Paint
             Graphics gPaint = e.Graphics;
 
-            //wyczyszczenie t³a
-            gPaint.Clear(Color.Yellow);
+            //ustawienie t³a
+            gPaint.Clear(Color.DarkGray);
             
             //stworzenie pêdzla
-            Brush bBlue= new SolidBrush(Color.Blue);
-            Brush bGray= new SolidBrush(Color.Gray);
-
+            Brush bBlue= new SolidBrush(Color.Red);
 
             //stworzenie pióra
-            Pen penRed = new Pen(Color.Red);
-            Pen penZielonyGruby = new Pen(Color.Green, 10);
+            Pen penZielony10 = new Pen(Color.Green, 10);
 
-/*
-            Rectangle rect = new Rectangle(50, 30, 100, 100);
-            LinearGradientBrush lBrush = new LinearGradientBrush(rect, Color.Red, Color.Yellow, LinearGradientMode.BackwardDiagonal);
-            g.FillRectangle(lBrush, rect);
-*/
+            //przygotowuje punkty
+            Point p1=new Point(10, 10);
+            Point p2=new Point(50, 10);
+            Point p3=new Point(100,10);
+            Point p4=new Point(300,100);
+            Point p5=new Point(500,100);
+            //tablica punktów
+            Point[] punkty1 = { p2, p5 }; 
+            Point[] punkty2 = { p2, p3,p5,p4 };
 
+
+            //rysuje odcinek
+            gPaint.DrawLine(penZielony10, 10, 20, 100, 200);
+            gPaint.DrawLine(penZielony10, p4, p5);
+
+            //elipsa
+            gPaint.DrawEllipse(penZielony10, 300,300, 200, 100 );
+
+            //prostok¹t
+            gPaint.DrawRectangle(penZielony10, 300, 500, 100, 50);
 
         }
     }
